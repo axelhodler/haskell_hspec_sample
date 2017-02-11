d434d320470c1efa08d57e781e7525f930c85ed3
Axel Hodler <derxor@gmail.com>
2017-02-11 17:40:23 +0100

    add quickcheck support

--- AdditionTest.hs
+++ AdditionTest.hs
@@ -2,9 +2,12 @@ module AdditionTest where
 
 import Addition
 import Test.Hspec
+import Test.QuickCheck
 
 test :: IO ()
-test = hspec $ do
+test = hspec $
   describe "Addition" $ do
-    it "1 + 1 is 2" $ do
+    it "1 + 1 is 2" $
       add 1 1 `shouldBe` 2
+    it "will always calculate x + 1 as greater than x" $
+      property $ \x -> x + 1 > (x :: Int)
